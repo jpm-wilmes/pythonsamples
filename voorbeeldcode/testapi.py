857dcb0a8ae3d616a68065441e05c5ba2b4a12b8
Jeroen Wilmes <58515763+jpm-wilmes@users.noreply.github.com>
2023-02-24 15:27:57 +0100

    up date

--- voorbeeldcode/testapi.py
+++ voorbeeldcode/testapi.py
@@ -4,16 +4,23 @@
 # getvariabele key: ?ovio-api-key=0b8e03b7e03ba082be304878df9c78cfaf13ad91d06fdf2decffd780d77f0c9e
 
 import requests
-import json
 
-KENTEKEN    = '/P280KK'
-URL         = 'https://api.overheid.io/voertuiggegevens'
+URL         = 'https://api.overheid.io/voertuiggegevens/'
 PARAMS      = {'ovio-api-key': '0b8e03b7e03ba082be304878df9c78cfaf13ad91d06fdf2decffd780d77f0c9e', \
                }
-# haal de gegevens op via de api
-response = requests.get(url = URL + KENTEKEN , params = PARAMS)
-# laat inhoud zien in json format
-data = response.json()
-print(data['handelsbenaming'])
-print(data['eerste_kleur'])
+kenteken = input("Geef kenteken")
+kenteken = kenteken.upper()
 
+# haal de gegevens op via de api
+response = requests.get(url = URL + kenteken , params = PARAMS)
+data = response.json()
+# laat inhoud zien in json format als kenteken bestaat
+print("**********************************")
+if response.text.find("error") > 0 :
+    print("* Kenteken   " + kenteken + " niet gevonden")
+else:
+    print("* KENTEKEN   " + kenteken)
+    print("* Merk       " + data['handelsbenaming'])
+    print("* Kleur      " + data['eerste_kleur'])
+    print("* APK        " + data['vervaldatum_apk'])
+print("**********************************")
